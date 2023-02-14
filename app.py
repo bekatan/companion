772c264c32021e3273211d43ad13bffac91695ee
Bekatan <bekatans@gmail.com>
2023-02-14 20:19:47 +0900

    -customized to generate empathetic responses

--- app.py
+++ app.py
@@ -10,26 +10,26 @@ openai.api_key = os.getenv("OPENAI_API_KEY")
 @app.route("/", methods=("GET", "POST"))
 def index():
     if request.method == "POST":
-        animal = request.form["animal"]
+        issue = request.form["issue"]
         response = openai.Completion.create(
-            model="text-davinci-002",
-            prompt=generate_prompt(animal),
-            temperature=0.6,
+            model="text-davinci-003",
+            prompt=generate_prompt(issue),
+            temperature=1.0,
+            max_tokens=100,
+            top_p=1.0,
         )
+        # print(response.choices[0].text)
         return redirect(url_for("index", result=response.choices[0].text))
 
     result = request.args.get("result")
     return render_template("index.html", result=result)
 
-
-def generate_prompt(animal):
-    return """Suggest three names for an animal that is a superhero.
-
-Animal: Cat
-Names: Captain Sharpclaw, Agent Fluffball, The Incredible Feline
-Animal: Dog
-Names: Ruff the Protector, Wonder Canine, Sir Barks-a-Lot
-Animal: {}
-Names:""".format(
-        animal.capitalize()
+def generate_prompt(issue):
+    return """example: 
+        issue: I am sad
+        response: It's okay to feel sad sometimes. You can talk to your close friends and family. 
+        Try doing things that make you happy like taking a walk or treating yourself with some sweets.
+        issue: \"{}\"
+        response: """.format(
+        issue.capitalize()
     )
